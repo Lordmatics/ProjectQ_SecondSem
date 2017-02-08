4ebb8fa012d2e119f81ca4f6807f6f9cf1ea6f65
Lordmatics <niallmaple@yahoo.co.uk>
2017-02-08 19:12:22 +0000

    Should of refactorred folder structure in code
    CPP + Header Split

--- Source/Quack/Headers/AIEnemies/QuackAIChargePawn.cpp
+++ Source/Quack/Headers/AIEnemies/QuackAIChargePawn.cpp
@@ -1,8 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Headers/Quack.h"
-#include "QuackAIChargePawn.h"
+#include "Headers/AIEnemies/QuackAIChargePawn.h"
 
 //HE THINKS HE CAN FACE ME IN SOLO QUEUE?????? ME?????
 
-
+// LUL
