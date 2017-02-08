ac2182f3af324afc3ccc164a454560a403f298b9
GachiPC\Ed <edward.hawksworth@outlook.com>
2017-02-08 19:04:25 +0000

    QuackAIChargePawn class added

---
+++ Source/Quack/Headers/AIEnemies/QuackAIChargePawn.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Headers/Quack.h"
+#include "QuackAIChargePawn.h"
+
+//HE THINKS HE CAN FACE ME IN SOLO QUEUE?????? ME?????
+
+
